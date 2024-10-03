cc8b6579e5f22be8b2be677d71d1464dbaa3ed03
Joy <yuusufjawadolamide67@gmail.com>
2024-10-03 22:25:21 +0100

    commit with Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
   
       <main className="flex flex-col min-h-screen bg-[#121212]">
-      
+      <Navbar />
       <div className="container mx-auto px-12 py-4">
         <HeroSection />
       </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Link from 'next/link'
+import NavLink from './NavLink'
+
+const navLinks = [
+    {
+        title: "About",
+        path: "#about"
+    },
+
+    {
+        title: "Projects",
+        path: "#projects"
+    },
+
+    {
+        title: "Contact",
+        path: "#contact"
+    },
+]
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
+                <Link href={"/"} className='text-5xl text-white font-semibold '>LOGO</Link>
+
+                <div className='menu md:block md:w-auto' id='navbar'>
+                    <ul>
+                        {
+                            navLinks.map((link, index)=>(
+                                <li key={index}>
+                                    <NavLink href={link.path} title={link.title} />
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
