c9348a1d9ccaa2559ca849b78d553c60ff8d1d7d
Joy <yuusufjawadolamide67@gmail.com>
2024-10-04 08:00:51 +0100

    Navbar and side bar toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import NavLink from './NavLink';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+import MenuOverLay from './MenuOverLay';
 
 const navLinks = [
     {
@@ -23,7 +24,7 @@ const NavBar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
 
     return (
-        <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+        <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
             <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
                 <Link href="/" className='text-2xl md:text-5xl text-white font-semibold'>
                     LOGO
@@ -52,6 +53,8 @@ const NavBar = () => {
                     </ul>
                 </div>
             </div>
+
+            {navbarOpen ? <MenuOverLay links={navLinks} /> : null}
         </nav>
     );
 };
