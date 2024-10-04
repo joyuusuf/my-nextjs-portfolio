96441b6dabd5d7756a2eaa21e1355d7c6bee8d83
Joy <yuusufjawadolamide67@gmail.com>
2024-10-04 04:44:50 +0100

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,12 +21,14 @@ const navLinks = [
 
 const NavBar = () => {
     return (
-        <nav>
-            <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
-                <Link href={"/"} className='text-5xl text-white font-semibold '>LOGO</Link>
+        <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'> 
+            <div className='flex flex-wrap items-center justify-between mx-auto px-4 '>
+                <Link href={"/"} className='text-2xl md:text-5xl text-white font-semibold '>
+                    LOGO
+                </Link>
 
                 <div className='menu md:block md:w-auto' id='navbar'>
-                    <ul>
+                    <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0 '>
                         {
                             navLinks.map((link, index)=>(
                                 <li key={index}>
