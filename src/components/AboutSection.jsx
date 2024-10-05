e6718c6f165d8b19ff11abd22892c9d76c7dd22c
Joy <yuusufjawadolamide67@gmail.com>
2024-10-05 21:50:53 +0100

    Another commit on AboutSection page

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,83 @@
+'use client';
+import React, {useTransition, useState} from 'react'
+import Image from 'next/image'
+import TabButton from './TabButton';
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>Bootstrap</li>
+      <li>React</li>
+      <li>Nextjs</li>
+      <li>Firebase</li> 
+      <li>Node.js</li>
+      <li>MongoDB</li>
+      <li>Git</li>
+      </ul>
+    )
+  },
+
+  {
+    title: "Education",
+    id: "education",
+    content:(
+      <ul>
+        <li>SQI College of ICT, Dugbe Campus, Ibadan, Oyo State, Nigeria.</li>
+        <li>Obafemi Awolowo University (OAU), Ile-Ife, Osun State, Nigeria. </li>
+      </ul>
+    )
+  },
+
+  {
+    title: "Experience"
+  }
+
+]
+const AboutSection = () => {
+  const [tab, setTab] = useState('skills');
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) =>{
+    startTransition(()=>{
+      setTab(id)
+    });
+  }
+  return (
+    <section className='text-white'>
+      <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
+        <img src="/images/abt-image.jpeg" alt="" width={500} height={500} />
+
+        <div>
+          <h2 className='text-4xl font-bold text-white mb-4 '>About Me</h2>
+          <p className='text-base lg:text-lg '>I am a full stack web developer with a passion for creating interactive and responsive web applications. I have experience working with HTML, CSS, Bootstrap, JavaScript, React, Next.Js, 
+              NodeJs, MongoDB, Git. I am a quick learner and 
+              I am always looking to expand my knowledge and skill set.
+              I am always excited to work with team to create amazing applications.
+          </p>
+          <div className='flex flex-row mt-8'>
+            <TabButton selectTab={()=> handleTabChange("skills")} active={tab==="skills"}> 
+              {" "}
+              Skills {" "}
+            </TabButton>
+
+            <TabButton selectTab={() => handleTabChange("education")} active={tab==="education"}> 
+              {" "}
+             Education {" "}
+            </TabButton> <TabButton selectTab={()=> handleTabChange("experience")} active={tab==="experiece"}> 
+              {" "}
+              Experience {" "}
+            </TabButton>
+           
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default AboutSection
