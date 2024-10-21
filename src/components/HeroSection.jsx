d48997d950d3a507d914c04fdfed95dc67d35f47
Joy <yuusufjawadolamide67@gmail.com>
2024-10-21 14:53:27 +0100

    link to my cv

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -55,7 +55,7 @@ const HeroSection = () => {
                     <div>
                         <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>Hire Me</button>
                         <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-r  from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
-                            <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px- py-2'>Download CV </span>
+                            <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px- py-2'><a href="https://we.tl/t-sip03eULSt">Download CV</a> </span>
                         </button>
                     </div>
 
